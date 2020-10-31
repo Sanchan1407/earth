bd5402949455c5aa9b9a8aadbd8da945df1e3cc0
Sanchan1407 <72304957+Sanchan1407@users.noreply.github.com>
2020-10-31 18:03:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const  $Climate=document.getElementById("Climate");
+const  $Consuption=document.getElementById("Consuption");
+const  $Resources=document.getElementById("Resources");
+const  $People=document.getElementById("People");
+
+$Climate.onclick= () =>{
+document.body.style.backgroundImage= "url('https://i1.faceprep.in/prograd-junior/climate-change-bg.png')"
+document.body.style.backgroundSize= "100% 100%"
+};
+
+$Consuption.onclick= () =>{
+    document.body.style.backgroundImage= "url ('https://i1.faceprep.in/prograd-junior/food-wastage-bg.png')"
+
+    document.body.style.backgroundSize= "100% 100%"
+    
+}
+
+$Resources.onclick= () =>{
+    document.body.style.backgroundImage= "url('https://i1.faceprep.in/prograd-junior/natural-resources-bg.png')"
+    document.body.style.backgroundSize= "100% 100%"
+    
+}
+
+$People.onclick= () =>{g
+    document.body.style.backgroundImage= "url('https://i1.faceprep.in/prograd-junior/poverty-bg.png')"
+    document.body.style.backgroundSize= "100% 100%"
+    
+}
